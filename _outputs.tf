c9daabcd67ea63d7200b45618b3cf8790f9e91c9
brianlycett <blycett@worth.systems>
2023-06-15 14:56:51 +0100

    Add function name as an output

--- _outputs.tf
+++ _outputs.tf
@@ -2,3 +2,8 @@ output "function_arn" {
   value       = aws_lambda_function.ingestion_function.arn
   description = "Log ingestion lambda function ARN"
 }
+
+output "function_name" {
+  value       = aws_lambda_function.ingestion_function.name
+  description = "Log ingestion lambda function name"
+}
